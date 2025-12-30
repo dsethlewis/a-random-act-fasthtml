5049e56c9c598250d9fa6f0d1b7d0ea332974cf7
Daniel Lewis <dsethlewis@gmail.com>
2025-12-30 09:40:36 -0800

    import example app

---
+++ main.py
@@ -0,0 +1,38 @@
+from fasthtml.common import (
+    fast_app, serve,
+    A, Card, Container, Group, P, Socials, Titled,
+    picolink,
+)
+
+app, rt = fast_app(hdrs=(picolink))
+
+
+@rt("/")
+def get():
+    summary = P(
+        "FastHTML is a new next-generation web framework for fast, scalable "
+        "web applications with minimal, compact code. It builds on top of "
+        "popular foundations like ASGI and HTMX. You can now deploy FastHTML "
+        "with Vercel CLI or by pushing new changes to your git repository.",
+    )
+    footer_content = P(
+        A(
+            "Deploy your own",
+            href="https://vercel.com/templates/python/fasthtml-python-boilerplate",
+        ),
+        " or ",
+        A("learn more", href="https://docs.fastht.ml/"),
+        " about FastHTML.",
+    )
+    return (
+        Container(
+            Card(
+                Group(summary),
+                header=(Titled("FastHTML + Vercel")),
+                footer=(footer_content),
+            ),
+        ),
+    )
+
+
+serve()
